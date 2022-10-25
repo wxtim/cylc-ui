a8fdeef36623c082208883da240675f5d9602e51
Tim Pillinger <26465611+wxtim@users.noreply.github.com>
2022-10-25 09:27:04 +0100

    doc links open in new tab or window

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -142,7 +142,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             </v-list-item-content>
           </v-list-item>
           <!-- TODO: change from latest to stable once out of beta -->
-          <v-list-item href="https://cylc.github.io/cylc-doc/latest/html/workflow-design-guide/index.html">
+          <v-list-item href="https://cylc.github.io/cylc-doc/latest/html/workflow-design-guide/index.html" target="_blank">
             <v-list-item-avatar size="60" style="font-size: 2em;">
               <v-icon large>{{ svgPaths.workflow }}</v-icon>
             </v-list-item-avatar>
@@ -156,7 +156,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             </v-list-item-content>
           </v-list-item>
           <!-- TODO: change from latest to stable once out of beta -->
-          <v-list-item href="https://cylc.github.io/cylc-doc/latest/html/index.html">
+          <v-list-item href="https://cylc.github.io/cylc-doc/latest/html/index.html" target="_blank">
             <v-list-item-avatar size="60" style="font-size: 2em;">
               <v-icon large>{{ svgPaths.documentation }}</v-icon>
             </v-list-item-avatar>
